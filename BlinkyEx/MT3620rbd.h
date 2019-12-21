af6ae8e77b3b57fa4414d0524b34627576f86f2b
Nicolas CLERC <nicolas.clerc@sablefin.net>
2019-12-21 22:14:31 +0100

    add defin in MT3620rbd.h for easy GPIO port naming flashing 4 leds ith french flag colors

---
+++ BlinkyEx/MT3620rbd.h
@@ -0,0 +1,30 @@
+#pragma once
+
+// Defining alias for GPIO in MT3620 reference design board (seeed)
+
+#define LED1_RED 8
+#define LED1_GREEN 9
+#define LED1_BLUE 10
+
+#define LED2_RED 15
+#define LED2_GREEN 16
+#define LED2_BLUE 17
+
+#define LED3_RED 18
+#define LED3_GREEN 19
+#define LED3_BLUE 20
+
+#define LED4_RED 21
+#define LED4_GREEN 22
+#define LED4_BLUE 23
+
+#define APPSTATUS_LED_RED 45
+#define APPSTATUS_LED_GREEN 46
+#define APPSTATUS_LED_BLUE 47
+
+#define WIFI_LED_RED 45
+#define WIFI_LED_GREEN 46
+#define WIFI_LED_BLUE 47
+
+
+
--- BlinkyEx/main.c
+++ BlinkyEx/main.c
@@ -6,38 +6,74 @@
 #include <applibs/log.h>
 #include <applibs/gpio.h>
 
-int main(void)
-{
-    // This minimal Azure Sphere app repeatedly toggles GPIO 9, which is the green channel of RGB
-    // LED 1 on the MT3620 RDB.
-    // If your device exposes different GPIOs, you might need to change this value. For example,
-    // to run the app on a Seeed mini-dev kit, change the GPIO from 9 to 7 in the call to
-    // GPIO_OpenAsOutput and in the app_manifest.json to blink its LED. Check with your hardware
-    // manufacturer to determine which GPIOs are available.
-    // Use this app to test that device and SDK installation succeeded that you can build,
-    // deploy, and debug an app with Visual Studio, and that you can deploy an app over the air,
-    // per the instructions here: https://docs.microsoft.com/azure-sphere/quickstarts/qs-overview
-    //
-    // It is NOT recommended to use this as a starting point for developing apps; instead use
-    // the extensible samples here: https://github.com/Azure/azure-sphere-samples
-    Log_Debug(
-        "\nVisit https://github.com/Azure/azure-sphere-samples for extensible samples to use as a "
-        "starting point for full applications.\n");
+#include "MT3620rbd.h"
 
-    // Change this GPIO number and the number in app_manifest.json if required by your hardware.
-    int fd = GPIO_OpenAsOutput(9, GPIO_OutputMode_PushPull, GPIO_Value_High);
-    if (fd < 0) {
+int gpioFD[4]; // gpio fd for the onbard leds (MT3260 dev board)
+
+/* InitGpioLeds(int gpioFD[])
+ *     initialize GPIO : LED 1/2/3/4
+ * params: 
+ *    gpiodFD : array of int , file descriptor for gpio leds
+ * return: 
+ *   -1 : error in intialization
+ *    0 : init succeeded. 
+*/
+int InitGpioLeds(int gpioFD[])
+{
+    gpioFD[0] = GPIO_OpenAsOutput(LED1_BLUE, GPIO_OutputMode_PushPull, GPIO_Value_High);
+    if (gpioFD[0] < 0) {
         Log_Debug(
             "Error opening GPIO: %s (%d). Check that app_manifest.json includes the GPIO used.\n",
             strerror(errno), errno);
         return -1;
     }
+    gpioFD[1] = GPIO_OpenAsOutput(LED2_GREEN, GPIO_OutputMode_PushPull, GPIO_Value_High);
+    if (gpioFD[0] < 0) {
+        Log_Debug(
+            "Error opening GPIO: %s (%d). Check that app_manifest.json includes the GPIO used.\n",
+            strerror(errno), errno);
+        return -1;
+    }
+    gpioFD[2] = GPIO_OpenAsOutput(LED3_RED, GPIO_OutputMode_PushPull, GPIO_Value_High);
+    if (gpioFD[0] < 0) {
+        Log_Debug(
+            "Error opening GPIO: %s (%d). Check that app_manifest.json includes the GPIO used.\n",
+            strerror(errno), errno);
+        return -1;
+    }
+    gpioFD[3] = GPIO_OpenAsOutput(LED4_GREEN, GPIO_OutputMode_PushPull, GPIO_Value_High);
+    if (gpioFD[0] < 0) {
+        Log_Debug(
+            "Error opening GPIO : %s (%d). Check that app_manifest.json includes the GPIO used.\n",
+            strerror(errno), errno);
+        return -1;
+    }
+
+    return 0;
+}
+
+void SetOnboardLedsValue(GPIO_Value led1, GPIO_Value led2, GPIO_Value led3, GPIO_Value led4)
+{
+    GPIO_SetValue(gpioFD[0], led1);
+    GPIO_SetValue(gpioFD[1], led2);
+    GPIO_SetValue(gpioFD[2], led3);
+    GPIO_SetValue(gpioFD[3], led4);
+}
+
+
+int main(void)
+{
+    if (InitGpioLeds(gpioFD) < 0)
+    {
+        Log_Debug("Error initializing GPIO Leds");
+        return -1;
+    }
 
     const struct timespec sleepTime = {1, 0};
     while (true) {
-        GPIO_SetValue(fd, GPIO_Value_Low);
+        SetOnboardLedsValue(GPIO_Value_Low, GPIO_Value_Low, GPIO_Value_Low, GPIO_Value_High);
         nanosleep(&sleepTime, NULL);
-        GPIO_SetValue(fd, GPIO_Value_High);
+        SetOnboardLedsValue(GPIO_Value_High, GPIO_Value_High, GPIO_Value_High, GPIO_Value_High);
         nanosleep(&sleepTime, NULL);
     }
 }
