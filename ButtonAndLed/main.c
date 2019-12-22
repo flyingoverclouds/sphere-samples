6cf804a31d707cbca2d84d6269d519a5ea1e7b11
Nicolas CLERC <nicolas.clerc@sablefin.net>
2019-12-22 18:39:04 +0100

    chagin timing using ns

---
+++ ButtonAndLed/main.c
@@ -0,0 +1,55 @@
+﻿#include <stdbool.h>
+#include <errno.h>
+#include <string.h>
+#include <time.h>
+
+#include <applibs/log.h>
+#include <applibs/gpio.h>
+
+#include "MT3620rbd.h"
+
+struct MyGpio {
+    int led1;
+    int buttonA;
+    int buttonB;
+};
+
+int InitGpios(struct MyGpio *gpios)
+{
+    gpios->led1 = GPIO_OpenAsOutput(LED1_GREEN, GPIO_OutputMode_PushPull, GPIO_Value_High);
+    if (gpios->led1 < 0) {
+        Log_Debug(
+            "Error opening GPIO: %s (%d). Check that app_manifest.json includes the GPIO used.\n",
+            strerror(errno), errno);
+        return -1;
+    }
+
+    gpios->buttonA = GPIO_OpenAsInput(BUTTON_A);
+    if (gpios->buttonA< 0) {
+        Log_Debug(
+            "Error opening GPIO: %s (%d). Check that app_manifest.json includes the GPIO used.\n",
+            strerror(errno), errno);
+        return -1;
+    }
+
+    gpios->buttonA = GPIO_OpenAsInput(BUTTON_B);
+    if (gpios->buttonA < 0) {
+        Log_Debug(
+            "Error opening GPIO: %s (%d). Check that app_manifest.json includes the GPIO used.\n",
+            strerror(errno), errno);
+        return -1;
+    }
+
+    return 0;
+}
+
+int main(void)
+{
+    struct MyGpio gpios;
+    InitGpios(&gpios);
+
+    const struct timespec sleepTime = {0, 100000};
+    while (true) {
+        nanosleep(&sleepTime, NULL);
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # sphere-samples
-Samples app for Azure Sphere
+Samples app for Azure Sphere (using the MT3620 Seeed Reference Dev Board )
 
-* BlinkyEx : a more advanced blinky sample
-
+* 01 __[BlinkyEx](~/BlinkyEx/)__ : a more advanced blinky sample with french flag colors on onboard led
+* 02 __ButtonAndLed__ : use onboard button to select and switch on/off onboard led
+* 03 __PwmWithLed__ : use a PWM channel to modulated on board led brightness
